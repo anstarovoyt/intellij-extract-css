1e3951d78489a740d9905d7282cffa7efcca15f4
GitHub Action <action@github.com>
2021-10-23 14:09:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/anstarovoyt/intellijextractcss/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.anstarovoyt.intellijextractcss.services
+
+import com.github.anstarovoyt.intellijextractcss.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
